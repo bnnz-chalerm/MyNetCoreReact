2577a3a1ccbbbb514ab1a248be44feb242462d0d
Chalerm Boonna <chalerm.bnn@gmail.com>
2017-09-12 16:08:37 +0700

    Init

---
+++ WebCoreReact/ClientApp/components/Hello.tsx
@@ -0,0 +1,15 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export class Hello extends React.Component<RouteComponentProps<{}>, {}> {
+    public render() {
+        return <div>
+            <h1>Hello Page</h1>
+            <p>Welcome to Hello Page</p>
+            
+        </div>;
+    }
+}
